f9fa9193043126dddf6309ee0fa88a7a90bd7025
Math3Faria <matheusteud12@gmail.com>
2025-05-28 11:02:52 -0300

    Deixando o código mais limpo

--- 03.js
+++ 03.js
@@ -1,6 +1,6 @@
 let numeros = [];
 let posicoesEncontradas = [];
-let numeroChave = parseInt(prompt("Digite o número que deseja saber a posição no vetor, entre 1 e 15:"));
+let numDigitado = parseInt(prompt("Digite o número que deseja saber a posição no vetor, entre 1 e 15:"));
 
 
 for (let i = 0; i < 30; i++) {
@@ -8,27 +8,27 @@ for (let i = 0; i < 30; i++) {
 }
 console.log(`Foram sorteados os números: ${numeros}`);
 
-if (isNaN(numeroChave) || numeroChave < 1 || numeroChave > 15) {
-    alert("Número inválido! Digite um número de 1 a 15.");
+if (isNaN(numDigitado) || numDigitado < 1 || numDigitado > 15) {
+    alert("Número inválido. Digite um número, e que seja de 1 a 15.");
 } else {
-    let contador = 0;
+    let qtdVezes = 0;
 
     for (let i = 0; i < numeros.length; i++) {
-        if (numeros[i] == numeroChave) {
-            posicoesEncontradas[contador] = i;
-            contador++;
+        if (numeros[i] == numDigitado) {
+            posicoesEncontradas[qtdVezes] = i;
+            qtdVezes++;
         }
     }
 
-    if (contador > 0) {
+    if (qtdVezes > 0) {
         let resultado = "" + posicoesEncontradas[0];
 
-        for (let i = 1; i < contador; i++) {
+        for (let i = 1; i < qtdVezes; i++) {
             resultado += ", " + posicoesEncontradas[i];
         }
 
-        alert(`O número ${numeroChave} apareceu ${contador} vezes nas posições: ${resultado}`);
+        alert(`O número ${numDigitado} apareceu ${qtdVezes} vezes nas posições: ${resultado}`);
     } else {
-        alert(`O número ${numeroChave} não foi encontrado no vetor.`);
+        alert(`O número ${numDigitado} não foi encontrado no vetor.`);
     }
 }
