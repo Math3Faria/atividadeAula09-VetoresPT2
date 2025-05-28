b1b85624e484e0841dee089a941acb6738b52203
Math3Faria <matheusteud12@gmail.com>
2025-05-28 11:21:48 -0300

    Deixando o código mais limpo

--- 03.js
+++ 03.js
@@ -1,12 +1,14 @@
 let numeros = [];
 let posicoesEncontradas = [];
-let numDigitado = parseInt(prompt("Digite um número entre 1 e 15 para saber o local que ele esta no vetor: "));
+let numDigitado;
 
 
 for (let i = 0; i < 30; i++) {
     numeros[i] = Math.ceil(Math.random() * 15);
 }
+
 console.log(`Foram sorteados os números: ${numeros}`);
+numDigitado = parseInt(prompt("Digite um número entre 1 e 15 para saber o local que ele esta no vetor: "));
 
 if (isNaN(numDigitado) || numDigitado < 1 || numDigitado > 15) {
     alert("Número inválido. Digite um número, e que seja de 1 a 15.");
